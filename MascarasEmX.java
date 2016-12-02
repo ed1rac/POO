ecf5ef6d89772269ce7ef8d3a496374ed641a0f9
Sham Vinicius Fiorin <skatesham@yahoo.com.br>
2016-12-02 17:53:22 -0200

    Lista Exercicio 1 - Atividade 3
    Mascara Em X

---
+++ MascarasEmX.java
@@ -0,0 +1,15 @@
+package MascarasAtividade3;
+
+public class MascarasEmX {
+
+	public static void main(String[] args) {
+		int i;
+		String espaco = "";
+		for (i=0; i<50; i++){
+			System.out.println(espaco + "X");
+			espaco += "  ";
+		}
+
+	}
+
+}
