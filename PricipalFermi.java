0d3db07cec8d33526481e32d1000616b1d532484
Sham Vinicius Fiorin <skatesham@yahoo.com.br>
2016-12-03 02:03:34 -0200

    Jogo Fermi - Classe Principal Trabalho Poo 2016/2
    P2 Trabalho de Poo
    Jogo Fermi

--- PricipalFermi.java
+++ PricipalFermi.java
@@ -1,24 +1,37 @@
 package FermiGame;
 
 import javax.swing.JOptionPane;
-
+/** Classe que contem a função main que executa o código
+ * 
+ * @author Sham Vinicius Fiorin
+ * @version 1.0
+ */
 public class PricipalFermi {
-
+	/** Metodo principal do programa
+	 * 
+	 * @param args String[]
+	 */
 	public static void main(String[] args) {
-		System.out.println("Benvindo ao Fermi Tente adivinhar...");
 		Fermi jogo = new Fermi();
 		int i = 1;
 		int tentativas = 0;
 		while(i != 0){
+			System.out.println("Chute |     Dica      |");
 			tentativas++;
-			int a = Integer.parseInt(JOptionPane.showInputDialog("Entre com um numero de 3 casa"));
-			int b = Integer.parseInt(JOptionPane.showInputDialog("Entre com um numero de 3 casa"));
-			int c = Integer.parseInt(JOptionPane.showInputDialog("Entre com um numero de 3 casa"));
+			int a = Integer.parseInt(JOptionPane.showInputDialog("Tentativa 1: "));
+			int b = Integer.parseInt(JOptionPane.showInputDialog("Tentativa 2: "));
+			int c = Integer.parseInt(JOptionPane.showInputDialog("Tentativa 3: "));
 			jogo.tentativa(a);
 			jogo.tentativa(b);
 			jogo.tentativa(c);
 			System.out.println("Tentativas: "+tentativas);
-			i = Integer.parseInt(JOptionPane.showInputDialog("Pressione '0' para sair"));
+			if(jogo.victory){
+				System.out.print("PARABENS VOCÊ GANHOU, RESPOSTA: ");
+				jogo.mostrar();
+				System.out.println("\n Novo Jogo Iniciado!");
+				jogo = new Fermi();
+			}
+			i = Integer.parseInt(JOptionPane.showInputDialog("0 PARA SAIR ou 1 PARA CONTINUAR"));
 		}
 		jogo.mostrar();
 		
